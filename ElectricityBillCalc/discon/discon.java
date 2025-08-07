787c72fb43786186377bf7b0d422926f0c665795
Vinay <vnmeight@gmail.com>
2025-08-07 09:50:39 +0530

    Create discon.java

---
+++ ElectricityBillCalc/discon/discon.java
@@ -0,0 +1,20 @@
+package discon;
+
+public class discon {
+
+  public double meterToKm(double meters) {
+  return meters / 1000;
+  }
+
+  public double kmToMeter(double km) {
+  return km * 1000;
+  }
+
+  public double milesToKm(double miles) {
+  return miles * 1.60934;
+  }
+
+  public double kmToMiles(double km) {
+  return km / 1.60934;
+  }
+}
