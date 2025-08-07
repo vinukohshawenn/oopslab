598fe4dc3281c456cbbbd2ec178f254d5397ee59
Vinay <vnmeight@gmail.com>
2025-08-07 09:47:33 +0530

    Create cc.java

---
+++ ElectricityBillCalc/cc/cc.java
@@ -0,0 +1,27 @@
+package cc;
+public class cc {
+  
+  public double dollarToInr(double dollars) {
+  return dollars * 83.50;
+  }
+
+  public double inrToDollar(double inr) {
+  return inr / 83.50;
+  }
+
+  public double euroToInr(double euros) {
+  return euros * 90.75;
+  }
+
+  public double inrToEuro(double inr) {
+  return inr / 90.75;
+  }
+
+  public double yenToInr(double yen) {
+  return yen * 0.53;
+  }
+
+  public double inrToYen(double inr) {
+  return inr / 0.53;
+  }
+}
