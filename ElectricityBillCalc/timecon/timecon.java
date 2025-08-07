9e8aa481e2e3160d91d56c61def04e872b3f4fb0
Vinay <vnmeight@gmail.com>
2025-08-07 09:56:42 +0530

    Create timecon.java

---
+++ ElectricityBillCalc/timecon/timecon.java
@@ -0,0 +1,21 @@
+package timecon;
+
+public class timecon {
+
+  public double hoursToMinutes(double hours) {
+  return hours * 60;
+  }
+
+  public double minutesToHours(double minutes) {
+  return minutes / 60;
+  }
+
+  public double hoursToSeconds(double hours) {
+  return hours * 3600;
+  }
+
+  public double secondsToHours(double seconds) {
+  return seconds / 3600;
+  }
+
+}
